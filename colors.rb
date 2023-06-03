fe5167f099eac217493fe2347f83455d32910cb6
Stefano Rasi <rasi.stefano@gmail.com>
2023-06-03 11:20:45 +0200

    Add height support

--- colors.rb
+++ colors.rb
@@ -8,6 +8,58 @@ class ColorsCard < SlimCard
     size 'B8'
 
     def initialize(node)
-        @colors = YAML.load(node.text)
+        colors = YAML.load(node.text)
+
+        @colors = colors.map do |row|
+            if row.respond_to? :key
+                color = row.keys.first
+
+                height = row.values.first
+
+                if height.is_a? String
+                    percentage = height.to_f
+
+                    { color: color, percentage: percentage }
+                else
+                    proportion = height.to_f
+
+                    { color: color, proportion: proportion }
+                end
+            else
+                color = row
+
+                proportion = 1.0
+
+                { color: color, proportion: proportion }
+            end
+        end
+
+        proportion_sum = @colors.reduce(0) { |sum, row|
+            if row.key? :proportion
+                sum + row[:proportion]
+            else
+                sum
+            end
+        }
+
+        total_percentage = @colors.reduce(0) { |sum, row|
+            if row.key? :percentage
+                sum + row[:percentage]
+            else
+                sum
+            end
+        }
+
+        remaining_percentage = 100.0 - total_percentage
+
+        proportion_percentage = remaining_percentage / proportion_sum
+
+        @colors.each do |row|
+            if row.key? :proportion
+                percentage = row[:proportion] * proportion_percentage
+
+                row[:percentage] = percentage
+            end
+        end
     end
 end
