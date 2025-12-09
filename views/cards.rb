092d970740b14248623f15a505a7299d4e4b6927
Stefano Rasi <rasi.stefano@gmail.com>
2025-12-09 07:07:44 +0100

    Create CardView

--- views/cards.rb
+++ views/cards.rb
@@ -23,14 +23,18 @@ class CardsView < View
     render do
         HTML.div :cards do
             @cards.each do |card|
-                HTML.div :card_container do |card_container|
+                HTML.div :card do |card_div|
                     data 'id', card['id']
 
                     HTTP.get("/cards/#{card['id']}/html") do |body|
-                        card_container.innerHTML = body
+                        card_div.innerHTML = body
                     end
                 end
             end
         end
+    end
+
+    class CardView < View
+        def initialize(id, type_id, text)
     end
 end
