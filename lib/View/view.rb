e2813f4c202525c8b3959d673ce74e0d16a8c26d
Stefano Rasi <rasi.stefano@gmail.com>
2025-12-14 05:42:22 +0100

    Add View builder method

--- lib/View/view.rb
+++ lib/View/view.rb
@@ -1,6 +1,10 @@
 require 'opal'
 
+require_relative 'html'
+
 class View
+    alias_method :old_method_missing, :method_missing
+
     def self.render(&block)
         if block_given?
             @render_block = block
@@ -14,6 +18,34 @@ class View
             @destroy_block = block
         else
             @destroy_block
+        end
+    end
+
+    def self.method_missing(name, *args, &block)
+        if /[A-Z]/.match(name)
+            if self.class.const_defined?(name)
+                klass = self.class.const_get(name)
+
+                if klass < View
+                    view_class = self.class.const_get(name)
+
+                    view = view_class.new(*args, &block)
+
+                    if HTML.element
+                        HTML.element.appendChild(view.element)
+                    end
+
+                    if block_given?
+                        block.call(view)
+                    end
+
+                    view
+                else
+                    old_method_missing(name, *args, &block)
+                end
+            else
+                old_method_missing(name, *args, &block)
+            end
         end
     end
 
