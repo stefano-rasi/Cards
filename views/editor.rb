7c9153914e5b3066da490ddc4a02abada3cd06ea
Stefano Rasi <rasi.stefano@gmail.com>
2025-12-26 20:23:11 +0100

    Clean up

--- views/editor.rb
+++ views/editor.rb
@@ -10,33 +10,21 @@ class EditorView < View
             HTML.div 'top-row' do
                 HTML.div 'type' do
                     HTML.select do |type_select|
+                        @type_select = type_select
+
                         required
 
                         if !@type
                             HTML.option text: 'type', value: '', disabled: true, selected: true
-                        end
-
-                        @type_select = type_select
-
-                        HTTP.get('/types') do |body|
-                            types = JSON.load(body)
-
-                            types.sort.each do |type|
-                                HTML.option text: type, value: type do |option|
-                                    selected if type == @type
-
-                                    @type_select.appendChild(option)
-                                end
-                            end
                         end
                     end
                 end
 
                 HTML.div 'attributes' do
                     HTML.input placeholder: 'attributes' do |input|
-                        value @attributes
-
                         @attributes_input = input
+
+                        value @attributes
                     end
                 end
 
@@ -49,37 +37,22 @@ class EditorView < View
 
             HTML.div 'text' do
                 HTML.textarea do |text_textarea|
+                    @text_textarea = text_textarea
+
                     value @text
 
                     placeholder 'text'
 
-                    @text_textarea = text_textarea
-
-                    @text_textarea.setAttribute('spellcheck', false)
+                    text_textarea.setAttribute('spellcheck', false)
                 end
             end
 
             HTML.div 'bottom-row' do
                 HTML.div 'binder' do
                     HTML.select do |binder_select|
-                        HTML.option text: 'binder', value: ''
-
                         @binder_select = binder_select
 
-                        HTTP.get('/binders') do |body|
-                            binders = JSON.load(body)
-
-                            binders.each do |binder|
-                                id = binder['id']
-                                name = binder['name']
-
-                                HTML.option text: name, value: id do |option|
-                                    selected if id == @binder_id
-
-                                    @binder_select.appendChild(option)
-                                end
-                            end
-                        end
+                        HTML.option text: 'binder', value: ''
                     end
                 end
             end
@@ -91,6 +64,33 @@ class EditorView < View
         @text = text
         @binder_id = binder_id
         @attributes = attributes
+
+        HTTP.get('/types') do |body|
+            types = JSON.load(body)
+
+            types.sort.each do |type|
+                HTML.option text: type, value: type do |option|
+                    selected if type == @type
+
+                    @type_select.appendChild(option)
+                end
+            end
+        end
+
+        HTTP.get('/binders') do |body|
+            binders = JSON.load(body)
+
+            binders.each do |binder|
+                id = binder['id']
+                name = binder['name']
+
+                HTML.option text: name, value: id do |option|
+                    selected if id == @binder_id
+
+                    @binder_select.appendChild(option)
+                end
+            end
+        end
     end
 
     def on_close(&block)
